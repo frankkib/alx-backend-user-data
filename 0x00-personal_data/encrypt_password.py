67c10c0a0dc6cc0e11af0815210429eee53ea315
frankkib <frankkibagendi13@gmail.com>
2023-08-31 09:50:13 +0000

    validating the hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,9 @@ def hash_password(password: str) -> bytes:
 
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates the password against a hashed using bcrypt.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

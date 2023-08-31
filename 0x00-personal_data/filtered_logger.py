b6fd0785b51562ac8209cbeccfd0f185c080c85b
frankkib <frankkibagendi13@gmail.com>
2023-08-31 09:06:03 +0000

    fixed the requirement error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,57 @@
 #!/usr/bin/env python3
 """
-function called filter_datum that returns the log message obfuscated
+This script demonstrates obfuscating sensitive fields in log messages.
 """
+
+import logging
+import csv
+import os
 import re
+from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Formatter class to obfuscate sensitive information in log messages.
+    """
+    REDACTION = "***"
+    SEPARATOR = ";"
+
+    def __init__(
+            self, fields: List[str], format_str: str = logging.BASIC_FORMAT):
+        super().__init__("[HOLBERTON] %(name)s%(levelname)s %(asctime)-15s: %(message)s")
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record while obfuscating specified fields.
+        """
+        message = super().format(record)
+        for field in self.fields:
+            pat = fr'(?<={re.escape(field)}=)[^{re.escape(self.SEPARATOR)}]+'
+            message = re.sub(pat, self.REDACTION, message)
+        return message
+
+
+PII_FIELDS: List[str] = ['name', 'email', 'phone', 'ssn', 'password']
+
+
+def get_logger() -> logging.Logger:
+    """
+    Create and configure a logger instance.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.addHandler(console_handler)
+    return logger
 
 
 def filter_datum(fields, redaction, message, separator):
     """
-    specified fields in a log message using regular expression
+    Obfuscate specified fields in a log message using regular expressions.
     Returns:
         str: The log message with specified fields obfuscated.
     """

0e2ac9a46e9d7ad39f26e53a6d31f4ac48710723
frankkib <frankkibagendi13@gmail.com>
2023-09-05 09:39:28 +0000

    updating the required function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,13 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determinis if authentication is required for a give path
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        slash = path + '/'
+
+        return path not in excluded_paths and slash not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Etracts the authorization header from the request

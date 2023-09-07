5a01d66e64fdffa5ee1ce1821935d7923bf00871
frankkib <frankkibagendi13@gmail.com>
2023-09-07 11:31:45 +0000

    funtion getting session id associated with the user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,3 +17,11 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Get the user ID associated with a given Session ID.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        user_id = self.user_id_by_session_id.get(session_id)
+        return user_id

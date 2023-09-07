17d137f3579f6a0f3d4f60244976d81abbab9e28
frankkib <frankkibagendi13@gmail.com>
2023-09-07 10:40:53 +0000

    updated the user route to session based

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if 'AUTH_TYPE' in os.environ:
+    auth_type = os.environ['AUTH_TYPE']
+    if auth_type == 'basic_auth':
+        auth = BasicAuth()
+    else:
+        auth = Auth()
+
+exempted_paths = [
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+
+@app.before_request
+def before_request():
+    """auth type"""
+    request.current_user = auth.current_user(request)
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """Unauthorized handler
+    """
+    return jsonfy({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden_error(error):
+    """forbidden error handler
+    """
+    return jsonify({"error": "Forbidden"})
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)

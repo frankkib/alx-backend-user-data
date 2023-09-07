d4e0377245772679dd72c2ea69c3cc0a99e1d0ee
frankkib <frankkibagendi13@gmail.com>
2023-09-07 07:36:04 +0000

    added the base64 function etractor

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,13 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """The basic Authentication class"""
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Etracts base64 Authorization headers
+        """
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]

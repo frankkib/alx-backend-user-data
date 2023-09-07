d9130ceceb2ddce0b3f15068304adf2ba42bfd13
frankkib <frankkibagendi13@gmail.com>
2023-09-07 09:21:03 +0000

    loaded the new user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Basic auth class module"""
 from api.v1.auth.auth import Auth
+from models.user import User
+from models import db as user_db
+from typing import TypeVar
 import base64
 
 
@@ -46,3 +49,31 @@ class BasicAuth(Auth):
         user_email, user_pass = decoded_base64_authorization_header.split(
                 ':')
         return user_email, user_pass
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Returns the user instance based on the provided email ad password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        user = user_db.User.search(user_email)
+        if user is None:
+            return None
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retreives the User instance for a request using Basic Authentication
+        """
+        authorization_header = request.headers.get('Authorization' , None)
+
+        base64_auth_hearder = self.extract_base64_authorization_header(authorization_header)
+        decoded_auth_header = self.decode_base64_authorization_header(base64_auth_hearder)
+        user_email, user_pwd = self.etract_user_credentials(decoded_auth_header)
+        if user_email is None or user_pwd is None:
+            return Nonee
+        User = self.user_object_from_credentials(user_email, user_pwd)
+        return user

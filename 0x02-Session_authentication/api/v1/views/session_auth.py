ad6a5f5eb89a4b85f9c2a56a0fdfb7f24c6122a6
frankkib <frankkibagendi13@gmail.com>
2023-09-08 10:26:18 +0000

    function for login

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""session module views
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from api.v1.app import auth
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    Requests a login form
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({"email": email})
+    if user is None:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    reponse.set_cookie(os.environ.get("SESSION_NAME"), session_id)
+    return response, 200

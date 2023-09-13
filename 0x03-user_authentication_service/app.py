6e68bb148204ad03f0342005cfc7c12dd24505bd
frankkib <frankkibagendi13@gmail.com>
2023-09-13 09:45:51 +0000

    Created the registered endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,13 @@
 """
 The flask app module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
@@ -15,6 +18,22 @@ def welcome():
     """
     message = {"message": "Bienvenue"}
     return message
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def register_users():
+    """
+    Function that registers new users
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        existing_user = AUTH.register_user(email, password)
+        response_data = {"email": email, "message": "User created"}
+        return jsonify(response_data), 200
+    except ValueError:
+        error_message = {"message": "email already registered"}
+        return jsonify(error_message), 400
 
 
 if __name__ == "__main__":

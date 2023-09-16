cb9745aac25e190a55df1ffed24db7f00d55d3f8
frankkib <frankkibagend13@gmail.com>
2023-09-16 07:53:33 +0000

    Fixed the pycode error

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,7 +12,7 @@ class User(Base):
     """
     __tablename__ = 'users'
     id: int = Column(Integer, primary_key=True)
-    email:str = Column(String(250), nullable=False)
+    email: str = Column(String(250), nullable=False)
     hashed_password: str = Column(String(250), nullable=False)
     session_id: str = Column(String(250), nullable=False)
     reset_token: str = Column(String(250), nullable=True)
